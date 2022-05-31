1ec65302a48bc2fcd2a6848fc5a43a069d7044cc
Flávio Alvarenga <13731643+alvalenda@users.noreply.github.com>
2022-05-31 08:47:04 -0300

    Delete homework.c

--- homework.c
+++
@@ -1,4 +0,0 @@
-/* Programa que lê um vetor de reais usando a notação de ponteiros, através de uma função que recebe o vetor alocado e o seu tamanho. Mostrar o vetor alocado e o seu tamanho. Mostrar o maior número do vetor através de outra função.
-Leitura Void */
-
-
